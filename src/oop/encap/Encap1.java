19b5907a14c46cb1cdc1c3af67c2e89fa81d5c40
Nyfong <litongfong12@gmail.com>
2025-01-30 08:49:03 +0700

    data hiding compiletime erroe

---
+++ src/oop/encap/Encap1.java
@@ -0,0 +1,16 @@
+package oop.encap;
+class AP{
+    private final int a =1; //  data hiding
+
+}
+
+public class Encap1 {
+
+
+    public static void main(String[] args) {
+
+        AP m = new AP();
+        System.out.println("trying to call ap private" + m.a); // can not access control to a
+
+    }
+}
